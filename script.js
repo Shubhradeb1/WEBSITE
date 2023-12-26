c6bf27e33b591cfa901454e37c85603943106067
shubhradeb1 <shubhrodebsingharoy@gmail.com>
2023-12-26 11:03:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(45);
+document.getElementsByTagName("h1")[0].style.color = "red";
+document.getElementsByTagName("h1")[1].style.color = "yellow";
+document.getElementsByTagName("h1")[2].style.color = "gray";
